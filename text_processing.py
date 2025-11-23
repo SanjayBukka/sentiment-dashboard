2be26c574028b8cd96e7c4461fbb7d3b5e2c18cc
SanjayBukka <sanjaybukka28@gmail.com>
2025-11-23 15:15:32 +0530

    Added text cleaning functions with regular expressions

---
+++ text_processing.py
@@ -0,0 +1,10 @@
+import re
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"@\w+", "", text)
+    text = re.sub(r"#", "", text)
+    text = re.sub(r"[^a-z0-9\s]", " ", text)
+    text = re.sub(r"\s+", " ", text).strip()
+    return text
